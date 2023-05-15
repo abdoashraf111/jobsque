da90c798d24a4a77a7fcdcfaa28255720bde4741
abdelrahman ashraf <63114136+abdoashraf111@users.noreply.github.com>
2023-05-15 17:14:45 +0300

    job page

--- lib/CustomItems/Custom_job.dart
+++ lib/CustomItems/Custom_job.dart
@@ -8,65 +8,89 @@ class CustomJob extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 103,
-      decoration: BoxDecoration(
-color: Colors.green
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Row(children: [
+      child: Column(
+        children: [
+          Row(
+            children: [
               Image.asset("lib/images/Zoom Logo.png"),
-              const SizedBox(width: 16,),
+              const SizedBox(
+                width: 16,
+              ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Product Designer",style: TextStyle(
-                      color: Colors.black,fontSize: 18)),
-                  const SizedBox(height: 4,),
-                  const Text("Zoom • United States",style: TextStyle(
-                      color: Colors.black,fontSize: 12)),
+                children: const [
+                  Text("Product Designer",
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text("Zoom • United States",
+                      style: TextStyle(color: Colors.black, fontSize: 12)),
                 ],
               ),
-              Spacer(flex: 1,),
-              Icon(Icons.add_box_rounded,color: Colors.black,),
-            ],),
-            const Spacer(flex: 1,),
-            Row(
-              children: [
-                Container(
-                  height: 26,
-                  width: 73,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                    color:Color.fromRGBO (255, 255, 255, 0.14),),
-                  child: Center(child: Text("Fulltime",style: TextStyle(color: Colors.blue))),),
-                const Spacer(flex: 1,),
-                Container(
-                  height: 26,
-                  width: 73,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                    color: Color.fromRGBO (255, 255, 255, 0.14),),
-                  child: Center(child: Text("Remote",style: TextStyle(color: Colors.blue))),),
-                const Spacer(flex: 1,),
-                Container(
-                  height: 26,
-                  width: 73,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                    color: Color.fromRGBO (255, 255, 255, 0.14),
-                  ),
-                  child: Center(child: Text("Design",style: TextStyle(color: Colors.blue))),),
-                // const Spacer(flex: 1,),
-                Row(children: const [
-                  Text("\$12K-15K",style: TextStyle(color: Colors.green,fontSize: 20)),
-                  Text("/Month",style: TextStyle(color: Colors.black,fontSize: 12)),
-                ],)
-              ],
-            ),
-          ],
-        ),
+              Spacer(
+                flex: 1,
+              ),
+              Icon(
+                Icons.add_box_rounded,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 26,
+                width: 73,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  color: Color.fromRGBO(255, 255, 255, 0.14),
+                ),
+                child: Center(
+                    child:
+                        Text("Fulltime", style: TextStyle(color: Colors.blue))),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              Container(
+                height: 26,
+                width: 73,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  color: Color.fromRGBO(255, 255, 255, 0.14),
+                ),
+                child: Center(
+                    child:
+                        Text("Remote", style: TextStyle(color: Colors.blue))),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              Container(
+                height: 26,
+                width: 73,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  color: Color.fromRGBO(255, 255, 255, 0.14),
+                ),
+                child: Center(
+                    child:
+                        Text("Design", style: TextStyle(color: Colors.blue))),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              const Text("\$12K-15K",
+                  style: TextStyle(color: Colors.green, fontSize: 16)),
+              const Text("/Month",
+                  style: TextStyle(color: Colors.black, fontSize: 12)),
+            ],
+          ),
+        ],
       ),
     );
   }
